1b5d7abdf7bc8387d0f5c7b10aaebe25a90bd40a
asanchez412 <sanchezandres41997@gmail.com>
2020-04-25 21:12:55 -0300

    Polymorphic PrintRecipe

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,10 @@
+using System;
+using Full_GRASP_And_SOLID.Library;
+
+public class ConsolePrinter : IPrinter
+{
+    public void PrintRecipe(Recipe recipe)
+    {
+        Console.WriteLine(recipe.GetTextToPrint());
+    }
+}
